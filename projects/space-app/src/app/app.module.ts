4f0f19d6b2dd319e98858f1f3d92e1aa2425398e
tbprojects <info@tbprojects.pl>
2021-04-14 23:51:12 +0200

    added a2s-comm

--- projects/space-app/src/app/app.module.ts
+++ projects/space-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { A2sCommModule } from 'a2s-comm';
 import { API_URL } from 'space-api/tokens';
 import { environment } from '../environments/environment';
 
@@ -15,6 +16,7 @@ import { StartComponent } from './views/start/start.component';
     StartComponent
   ],
   imports: [
+    A2sCommModule,
     BrowserModule,
     SharedModule,
     HttpClientModule,
