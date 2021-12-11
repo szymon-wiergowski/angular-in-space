524635b63c3e5f3f5d402bdfd20df8ccff29b2ee
Szymon Wiergowski <szymon.wiergowski@advanfile.com>
2021-12-11 11:02:37 +0100

    mission-13

--- projects/space-app/src/app/app.component.ts
+++ projects/space-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { AppConfig } from 'space-api/types';
 import { APP_CONFIG } from './app-config-token';
 import { BusyInterceptor } from './services/busy.interceptor';
+import { UpdateService } from './services/update.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,16 @@ export class AppComponent {
   appVersion = this.appConfig.version;
   title = 'space-app';
   busy = this.busyInterceptor.busy;
+  updateAvailable = this.update.updateAvailable;
+  unrecoverableError = this.update.unrecoverableError;
 
-  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig, private busyInterceptor: BusyInterceptor) {}
+  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig, private busyInterceptor: BusyInterceptor, private update: UpdateService) {}
+
+  activateUpdate(): void {
+    this.update.activateUpdate();
+  }
+
+  reloadApp(): void {
+    this.update.reloadApp();
+  }
 }
